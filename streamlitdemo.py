ffaae742e8508c54a04f8af44ad391384ce4c6de
joelfranchini <43697860+joelfranchini@users.noreply.github.com>
2021-06-29 20:17:59 +0200

    Update streamlitdemo.py

--- streamlitdemo.py
+++ streamlitdemo.py
@@ -5,26 +5,38 @@ import streamlit as st
 
 
 st.write("""
-# My first app
-Hello *world!*
+# MoneyTracker v1.0
+Programma per gestire le *finanze* personali
 """)
 
 #d = st.date_input("When's your birthday") #di default mette la data di oggi
 #d = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
 #st.write('Your birthday is:', d)
 
-data=st.date_input("per il giorno")
+data=st.date_input("Movimento relativo al giorno (YYYY/MM/DD):")
 #data=st.date_input("per il giorno", value=None, key=None, help=None
-st.write(data)
+#st.write('Hai selezionato:', data)
 
 
 options = st.selectbox('Che tipo di movimento stai per introdurre?',('-----', 'Entrata', 'Uscita'))
-st.write('You selected:', options)
+#st.write('Hai selezionato:', options)
 if options=="-----":
     pass
 elif options=='Uscita':
     option_uscita = st.selectbox('Che tipo di uscita è?',('Mangiare fuori', 'Spesa base (benzina/alimenti)', 'Sfizi', 'Beneficienza', 'Caffè', 'Amazon', 'Regali obbligati'))
-    st.write('You selected:', option_uscita)
+    st.write('Hai selezionato una ',options,'avvenuta il giorno ',str(data),'con causale ',  option_uscita)
 else:
     option_entrata = st.selectbox('Che tipo di entrata è?',('Stipendio', 'Bonifico Parenti', 'PoliMi'))
-    st.write('You selected:', option_entrata)
+    st.write('Hai selezionato una ',options,', avvenuta il giorno ',str(data),', con causale ',  option_entrata)
+
+tripletta=st.radio("In che range è la cifra?", ('0-200 €','201-1000 €','1001-3000 €'))
+
+
+if tripletta=="0-200 €":
+    cifra=st.slider("Inserisci la cifra: ", 0,200)
+elif options=='201-1000 €':
+    cifra=st.slider("Inserisci la cifra: ", 201,1000)
+else:
+    cifra=st.slider("Inserisci la cifra: ", 1001,3000)
+
+st.write(str(cifra), '€')
